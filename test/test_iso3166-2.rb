a7176bc9fdd7dee068464c0f8383a158fadb32b2
Ernesto Tagwerker <ernesto@ombushop.com>
2013-05-07 15:38:46 +0200

    Simples test for regions.

--- test/test_iso3166-2.rb
+++ test/test_iso3166-2.rb
@@ -1,7 +1,22 @@
 require 'helper'
+require 'pry'
 
-class TestIso31662 < Test::Unit::TestCase
-  should "probably rename this file and start testing for real" do
-    flunk "hey buddy, you should probably rename this file and start testing for real"
+require "minitest/autorun"
+
+class TestIso31662 < MiniTest::Unit::TestCase
+
+  # Should return N countries
+  def test_regions
+    assert_equal ISO3166_2.regions.count, 4400
   end
+
+  def test_first_region
+    region = ISO3166_2.regions[0]
+    assert_equal region["iso"], "AD"
+    assert_equal region["country"], "Andorra"
+    assert_equal region["iso2"], "AD-02"
+    assert_equal region["region"], "Canillo"
+    assert_equal region["alt_name_1"], ''
+  end
+
 end
