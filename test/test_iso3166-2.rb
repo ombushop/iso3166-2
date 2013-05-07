d791a8779af4633eb773ef83aa03c737b10fe27b
Ernesto Tagwerker <ernesto@ombushop.com>
2013-05-07 13:49:01 +0200

    Initial commit to iso3166-2.

---
+++ test/test_iso3166-2.rb
@@ -0,0 +1,7 @@
+require 'helper'
+
+class TestIso31662 < Test::Unit::TestCase
+  should "probably rename this file and start testing for real" do
+    flunk "hey buddy, you should probably rename this file and start testing for real"
+  end
+end
